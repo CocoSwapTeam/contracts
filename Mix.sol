2409aa1c044cdddfae74d28cb33de8e21bcc9fd3
CocoSwapTeam <87112073+CocoSwapTeam@users.noreply.github.com>
2021-07-09 17:56:35 +0800

    Create Mix.sol

---
+++ Mix.sol
@@ -0,0 +1,449 @@
+// File: openzeppelin-solidity/contracts/math/SafeMath.sol
+
+pragma solidity ^0.5.0;
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+}
+
+// File: openzeppelin-solidity/contracts/ownership/Ownable.sol
+
+pragma solidity ^0.5.0;
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be aplied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * > Note: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+// File: openzeppelin-solidity/contracts/token/ERC20/IERC20.sol
+
+pragma solidity ^0.5.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
+ * the optional functions; to access them see `ERC20Detailed`.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a `Transfer` event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through `transferFrom`. This is
+     * zero by default.
+     *
+     * This value changes when `approve` or `transferFrom` are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * > Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an `Approval` event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a `Transfer` event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to `approve`. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+// File: contracts/CocoMix.sol
+
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+
+
+
+interface ERC20Interface {
+    function balanceOf(address user) external view returns (uint256);
+}
+
+library SafeToken {
+    function myBalance(address token) internal view returns (uint256) {
+        return ERC20Interface(token).balanceOf(address(this));
+    }
+
+    function balanceOf(address token, address user) internal view returns (uint256) {
+        return ERC20Interface(token).balanceOf(user);
+    }
+
+    function safeApprove(address token, address to, uint256 value) internal {
+        // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeApprove");
+    }
+
+    function safeTransfer(address token, address to, uint256 value) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeTransfer");
+    }
+
+    function safeTransferFrom(address token, address from, address to, uint256 value) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeTransferFrom");
+    }
+
+    function safeTransferETH(address to, uint256 value) internal {
+        (bool success,) = to.call.value(value)(new bytes(0));
+        require(success, "!safeTransferETH");
+    }
+}
+
+interface IWETH {
+    function balanceOf(address user) external returns (uint);
+
+    function approve(address to, uint value) external returns (bool);
+
+    function transfer(address to, uint value) external returns (bool);
+
+    function deposit() external payable;
+
+    function withdraw(uint) external;
+}
+
+interface ITrader {
+
+    struct Route {
+        address token0;
+        address[] token1;
+        address[] routerList;
+        uint256[] ratio;
+    }
+
+    function multiSwap(Route[] calldata _data) external;
+}
+
+interface IMiner {
+
+    function mint(address user, address token, uint256 amount, uint256 fee) external;
+}
+
+contract CocoMix is Ownable {
+    using SafeMath for uint256;
+
+    IWETH wETH = IWETH(address(0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F));
+
+    address trader = address(0);
+    address miner = address(0);
+    address feeReceiver = address(0);
+
+    uint256 BASE_FEE = 1000000;
+    uint256 DEFAULT_FEE = 200;
+    mapping (address => bool) public tokenWhiteList;
+    mapping (address => uint256) public tokenFee;
+
+    constructor(address _trader, address _minter, address _receiver) public {
+        trader = _trader;
+        miner = _minter;
+        feeReceiver = _receiver;
+    }
+
+    function getTokenFee(address token) public view returns (uint256) {
+        if(tokenWhiteList[token]){
+            return 0;
+        }
+        if(tokenFee[token] > 0 && tokenFee[token] < BASE_FEE){
+            return tokenFee[token];
+        }
+        return DEFAULT_FEE;
+    }
+
+    function multiSwap(address user, address inToken, uint256 amountIn, address outToken, uint256 minOut,
+        ITrader.Route[] calldata _data) external payable {
+
+        //Token in
+        if (inToken == address(0)) {
+            uint256 ethBalance = address(this).balance;
+            require(amountIn == ethBalance, "amountIn not equal");
+            wETH.deposit.value(ethBalance)();
+        } else {
+            SafeToken.safeTransferFrom(inToken, user, address(this), amountIn);
+        }
+
+        // Fee
+        uint256 feeRate = getTokenFee(inToken);
+        uint256 fee = 0;
+        if(feeRate > 0){
+            fee = amountIn.mul(feeRate).div(BASE_FEE);
+            require(fee > 0, 'fee need > 0');
+            if(inToken == address(0)){
+                wETH.transfer(feeReceiver, fee);
+            } else {
+                SafeToken.safeTransfer(inToken, feeReceiver, fee);
+            }
+        }
+
+        //Swap
+        if (inToken == address(0)) {
+            wETH.transfer(trader, amountIn-fee);
+        } else {
+            SafeToken.safeTransfer(inToken, trader, amountIn-fee);
+        }
+        ITrader(trader).multiSwap(_data);
+
+        //Minter
+        IMiner(miner).mint(user, inToken, amountIn, fee);
+
+        //Out
+        if (outToken == address(0)) {
+            uint256 ethBalance = wETH.balanceOf(address(this));
+            wETH.withdraw(ethBalance);
+            require(ethBalance > minOut, "less than minOut");
+            SafeToken.safeTransferETH(user, ethBalance);
+        } else {
+            uint256 balance = SafeToken.balanceOf(outToken, address(this));
+            require(balance > minOut, "less than minOut");
+            SafeToken.safeTransfer(outToken, user, balance);
+        }
+    }
+
+    function setTrader(address _trader) external onlyOwner {
+        trader = _trader;
+    }
+
+    function setMiner(address _miner) external onlyOwner {
+        miner = _miner;
+    }
+
+    function setFee(uint256 _fee) external onlyOwner {
+        DEFAULT_FEE = _fee;
+    }
+
+    function setTokens(address[] calldata _tokens, bool isOk, uint256 _fee) external onlyOwner {
+        for (uint256 idx = 0; idx < _tokens.length; idx++) {
+            tokenWhiteList[_tokens[idx]] = isOk;
+            tokenFee[_tokens[idx]] = _fee;
+        }
+    }
+
+    function recover(address token, address to, uint256 value) external onlyOwner {
+        if (token == address(0)) {
+            SafeToken.safeTransferETH(to, value);
+        } else {
+            SafeToken.safeTransfer(token, to, value);
+        }
+    }
+
+    function() external payable {}
+}
